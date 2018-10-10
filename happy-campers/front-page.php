12c8a890541dfa33b0ffeba8760723029e839a0b
corbecker <corbinbecker@protonmail.com>
2018-10-10 09:44:42 -0700

    nav styling

---
+++ happy-campers/front-page.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Custom front page template
+ *
+ *
+ * @package happycampers
+ */
+get_header();
+?>
+
+<div class="parallax-header">
+    <?php
+    the_custom_logo();
+    ?>
+</div><!-- .site-branding -->
+
+<?php
+get_footer();
