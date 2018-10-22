5f4abe1b6c4668cd00678a9acf4f0f56abfd39d3
corbecker <corbinbecker@protonmail.com>
2018-10-22 12:56:47 -0700

    updated tabs styling

--- happy-campers/header-services.php
+++ happy-campers/header-services.php
@@ -1,19 +1,21 @@
 <div class="masthead">
     <div class="container">
-        <h1><?php single_post_title(); ?></h1>
+        <h1>Services</h1>
     </div>
 </div>
 <nav class="sub-navigation">
-    <ul class="toggle">
-        <li class="fa fa-bars"></li>
-    </ul>
-    <?php
-    wp_nav_menu(
-        array(
-        'theme_location' => 'menu-2',
-        'menu_id'        => 'services-menu',
-        'container'      => 'false'
-        )
-    );
-    ?>
+    <div class="">
+        <ul class="toggle">
+            <li class="fa fa-bars"></li>
+        </ul>
+        <?php
+        wp_nav_menu(
+            array(
+            'theme_location' => 'menu-2',
+            'menu_id'        => 'services-menu',
+            'container'      => 'false'
+            )
+        );
+        ?>
+    </div>
 </nav>
--- happy-campers/page-general.php
+++ happy-campers/page-general.php
@@ -4,21 +4,20 @@ get_header();
 get_header('services');
 ?>
 <div class="main">
-<div class="main-sidebar container">
-    <div class="content">
-      Content here...
-    </div>
-    <div class="sidebar">
-        <div id="general-sidebar1" class="sidebar-widget">
-            <?php
-            if (is_active_sidebar('general-sidebar-1')) {
-                dynamic_sidebar('general-sidebar-1');
-            }
-            ?>
+    <div class="main-sidebar container card">
+        <div class="content">
+          <h2><?php single_post_title() ?></h2>
+            <?php echo get_post_field('post_content', $post->ID); ?>
+        <div class="sidebar">
+            <div id="general-sidebar1" class="sidebar-widget">
+                <?php
+                if (is_active_sidebar('general-sidebar-1')) {
+                    dynamic_sidebar('general-sidebar-1');
+                }
+                ?>
+            </div>
         </div>
     </div>
-</div>
-  
 </div>
 <?php
 get_footer();
